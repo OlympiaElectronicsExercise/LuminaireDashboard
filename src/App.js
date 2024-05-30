36aa9f484d5ed409ba221bf415a8bd1ffac40d4e
Dean Joanidhi <deanj.development@gmail.com>
2024-05-30 14:41:28 +0300

    implementation of react routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 
+//Standard Imports & Dependencies
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//Imports of components for initial user interaction
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+
+
+//Imports for MAIN MENU Components
+import Dashboard from './components/Dashboard/Dashboard';
+
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
+    <>
+    <Router>
+      {/* Routes for initial user interaction */}
+      <Route path='/Login' element={<Login/>}/>
+      <Route path='/Register' element={<Register/>}/>
+
+      {/* Routes for MAIN MENU components */}
+      <Route path='/Dashbaord' element={<Dashboard/>}/>
+
+      
+    </Router>
+    </>
   );
 }
 
