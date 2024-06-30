db29ae0872ff031582faabfd165617ab0b0f560e
george kalleas <georgekalleas@outlook.com>
2024-06-30 15:32:25 +0300

    Route Protection and Forgot Password (page)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 
 //Standard Imports & Dependencies
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from "react-router-dom";
 
 //Imports of components for initial user interaction
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 
 //Imports for MAIN MENU Components
 import Dashboard from "./components/Dashboard/Dashboard.tsx";
@@ -15,6 +16,15 @@ import SingleDevicePage from "./components/Device/SingleDevicePage.tsx";
 import ListPage from "./components/ListPage/ListPage.tsx";
 
 function App() {
+    function ProtectedRoute ({ isAuth, target }) {
+        const token = sessionStorage.getItem('token');
+      
+        if (isAuth) {
+          return token ? <Outlet /> : <Navigate to={target} replace />;
+        }
+        return token ? <Navigate to={target} replace /> : <Outlet />;
+      }
+
     return (
         <>
             <Router>
@@ -22,17 +32,21 @@ function App() {
                     {/* Routes for initial user interaction */}
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
 
                     {/* Routes for MAIN MENU components */}
-                    <Route path="/dashboard" element={<Dashboard />} />
 
-                    {/* Dynamic route for SingleDevicePage */}
-                    <Route
-                        path="/device/:deviceId"
-                        element={<SingleDevicePage />}
-                    />
+                    <Route element={<ProtectedRoute isAuth target="/login" />}>
+                        {/* Dynamic route for Dashboad */}
 
-                    <Route path="/list/:type" element={<ListPage />} />
+                        <Route path='/dashboard' element={<Dashboard />} />
+                    
+                        {/* Dynamic route for SingleDevicePage */}
+                        <Route path="/device/:deviceId" element={<SingleDevicePage />} />
+
+                        <Route path="/list/:type" element={<ListPage />} />
+                    </Route>
+
                     {/* Index Routing */}
                     <Route index element={<Login />} />
                 </Routes>
