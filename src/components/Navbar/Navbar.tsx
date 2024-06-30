03790fe5b0203d8985a6bdeac9519711033fee45
Dean Joanidhi <deanj.development@gmail.com>
2024-06-30 20:43:44 +0300

    addition of a navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import {useAuth} from '../../context/auth-context.tsx'
+
+const Navbar: React.FC = () => { 
+    const {isLoggedIn, logout} = useAuth();
+
+    const handleLogoff = () => {
+        console.log("Logging off..");
+        logout();
+    }
+
+    return (
+        <nav className="bg-gray-800 p-4 flex justify-between items-center text-white">
+            <div className="flex items-center">
+                <span className="mr-4 font-bold">Olympia Electronics</span>
+            </div>
+            {isLoggedIn ? (
+                <button
+                    onClick={handleLogoff}
+                    className="text-white bg-gray-600 hover:bg-gray-700 px-4 py-2 rounded cursor-pointer"
+                >
+                    Log Off
+                </button>
+            ) : null}
+        </nav>
+    );
+};
+
+export default Navbar;
